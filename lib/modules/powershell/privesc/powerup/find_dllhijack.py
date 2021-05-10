4009409caa35cd06719a0d7528dfffdd1ee50f5b
Jamaledine AMARIR <jamaledine.amarir@gmail.com>
2021-05-10 12:07:06 +0200

    Added PowerShell output option (powershell/privesc/powerup/find_dllhijack.py)
    self.options now contains the option "OuputFunction", allowing the user to choose the PowerShell function used to gen>
    * Out-String
    * ConvertTo-Json
    * ConvertTo-Csv
    * ConvertTo-Html
    * ConvertTo-Xml

--- lib/modules/powershell/privesc/powerup/find_dllhijack.py
+++ lib/modules/powershell/privesc/powerup/find_dllhijack.py
@@ -47,6 +47,11 @@ class Module(object):
                 'Required'      :   True,
                 'Value'         :   ''
             },
+            'OutputFunction' : {
+                'Description'   :   'PowerShell\'s output function to use ("Out-String", "ConvertTo-Json", "ConvertTo-Csv", "ConvertTo-Html", "ConvertTo-Xml").',
+                'Required'      :   False,
+                'Value'         :   'Out-String'
+            },
             'ExcludeWindows' : {
                 'Description'   :   "Switch. Exclude paths from C:\Windows\* instead of just C:\Windows\System32\*",
                 'Required'      :   False,
@@ -100,7 +105,7 @@ class Module(object):
         scriptEnd = ';' + moduleName + " "
 
         for option,values in self.options.items():
-            if option.lower() != "agent":
+            if option.lower() != "agent" and option.lower() != "outputfunction":
                 if values['Value'] and values['Value'] != '':
                     if values['Value'].lower() == "true":
                         # if we're just adding a switch
@@ -108,7 +113,8 @@ class Module(object):
                     else:
                         scriptEnd += " -" + str(option) + " " + str(values['Value']) 
 
-        scriptEnd += ' | ft -wrap | Out-String | %{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
+        outputf = self.options["OutputFunction"]["Value"]
+        scriptEnd += " | ft -wrap | {outputf} | ".format(outputf=outputf) + '%{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
 
         if obfuscate:
             scriptEnd = helpers.obfuscate(self.mainMenu.installPath, psScript=scriptEnd, obfuscationCommand=obfuscationCommand)
