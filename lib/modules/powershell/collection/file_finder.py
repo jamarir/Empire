2e3a0b575c35434221b773e4b8f58c47113edd3d
Jamaledine AMARIR <jamaledine.amarir@gmail.com>
2021-05-10 16:55:55 +0200

    Added PowerShell output option (powershell/collection/file_finder.py)
    self.options now contains the option "OuputFunction", allowing the user to choose the PowerShell
    function used to generate the Empire's output logs:
    * Out-String
    * ConvertTo-Json
    * ConvertTo-Csv
    * ConvertTo-Html
    * ConvertTo-Xml
    
    NB: The module works as excepted but my test shows an empty output as I have not setup the appropriate
    experimental lab.

--- lib/modules/powershell/collection/file_finder.py
+++ lib/modules/powershell/collection/file_finder.py
@@ -49,6 +49,11 @@ class Module(object):
                 'Required'      :   False,
                 'Value'         :   ''
             },
+            'OutputFunction' : {
+                'Description'   :   'PowerShell\'s output function to use ("Out-String", "ConvertTo-Json", "ConvertTo-Csv", "ConvertTo-Html", "ConvertTo-Xml").',
+                'Required'      :   False,
+                'Value'         :   'Out-String'
+            },
             'ComputerFilter' : {
                 'Description'   :   'Host filter name to query AD for, wildcards accepted.',
                 'Required'      :   False,
@@ -159,15 +164,15 @@ class Module(object):
         script += moduleName + " "
 
         for option,values in self.options.items():
-            if option.lower() != "agent":
+            if option.lower() != "agent" and option.lower() != "outputfunction":
                 if values['Value'] and values['Value'] != '':
                     if values['Value'].lower() == "true":
                         # if we're just adding a switch
                         script += " -" + str(option)
                     else:
                         script += " -" + str(option) + " " + str(values['Value']) 
-
-        script += ' | Out-String | %{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
+        outputf = self.options["OutputFunction"]["Value"]
+        script += " | {outputf} | ".format(outputf=outputf) + '%{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
 
         # Get the random function name generated at install and patch the stager with the proper function name
         if obfuscate:
