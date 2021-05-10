13c2e51d66961c583c22e47b8e6365f7c73117e1
Jamaledine AMARIR <jamaledine.amarir@gmail.com>
2021-05-10 11:45:16 +0200

    Added PowerShell output option (powershell/situational_awareness/network/powerview/find_localadmin_access.py)
    self.options now contains the option "OuputFunction", allowing the user to choose the PowerShell function used to generate the Empire's output logs:
    * Out-String
    * ConvertTo-Json
    * ConvertTo-Csv
    * ConvertTo-Html
    * ConvertTo-Xml

--- lib/modules/powershell/situational_awareness/network/powerview/find_localadmin_access.py
+++ lib/modules/powershell/situational_awareness/network/powerview/find_localadmin_access.py
@@ -48,6 +48,11 @@ class Module(object):
                 'Required'      :   True,
                 'Value'         :   ''
             },
+            'OutputFunction' : {
+                'Description'   :   'PowerShell\'s output function to use ("Out-String", "ConvertTo-Json", "ConvertTo-Csv", "ConvertTo-Html", "ConvertTo-Xml").',
+                'Required'      :   False,
+                'Value'         :   'Out-String'
+            },
             'ComputerName' : {
                 'Description'   :   'Hosts to enumerate, comma separated.',
                 'Required'      :   False,
@@ -148,7 +153,7 @@ class Module(object):
         script += "\n" + moduleName + " "
 
         for option,values in self.options.items():
-            if option.lower() != "agent":
+            if option.lower() != "agent" and option.lower() != "outputfunction":
                 if values['Value'] and values['Value'] != '':
                     if values['Value'].lower() == "true":
                         # if we're just adding a switch
@@ -156,7 +161,8 @@ class Module(object):
                     else:
                         script += " -" + str(option) + " " + str(values['Value']) 
         
-        script += ' | Out-String | %{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
+        outputf = self.options["OutputFunction"]["Value"]
+        script += " | {outputf} | ".format(outputf=outputf) + '%{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
 
         if obfuscate:
             script = helpers.obfuscate(self.mainMenu.installPath, psScript=script, obfuscationCommand=obfuscationCommand)
