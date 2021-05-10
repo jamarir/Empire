4f047429399e38ab7ae47cd1bf6ae1290b095c55
Jamaledine AMARIR <jamaledine.amarir@gmail.com>
2021-05-10 17:30:26 +0200

    Added PowerShell output option (powershell/management/get_domain_sid.py)
    self.options now contains the option "OuputFunction", allowing the user to choose the PowerShell
    function used to generate the Empire's output logs:
    * Out-String
    * ConvertTo-Json
    * ConvertTo-Csv
    * ConvertTo-Html
    * ConvertTo-Xml

--- lib/modules/powershell/management/get_domain_sid.py
+++ lib/modules/powershell/management/get_domain_sid.py
@@ -45,6 +45,11 @@ class Module(object):
                 'Required'      :   True,
                 'Value'         :   ''
             },
+            'OutputFunction' : {
+                'Description'   :   'PowerShell\'s output function to use ("Out-String", "ConvertTo-Json", "ConvertTo-Csv", "ConvertTo-Html", "ConvertTo-Xml").',
+                'Required'      :   False,
+                'Value'         :   'Out-String'
+            },
             'Domain' : {
                 'Description'   :   'Domain to resolve SID for, defaults to the current domain.',
                 'Required'      :   False,
@@ -84,7 +89,7 @@ class Module(object):
         script += moduleName + " "
         scriptEnd = ""
         for option,values in self.options.items():
-            if option.lower() != "agent":
+            if option.lower() != "agent" and option.lower() != "outputfunction":
                 if values['Value'] and values['Value'] != '':
                     if values['Value'].lower() == "true":
                         # if we're just adding a switch
@@ -92,7 +97,8 @@ class Module(object):
                     else:
                         scriptEnd += " -" + str(option) + " " + str(values['Value']) 
 
-        scriptEnd += ' | Out-String | %{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
+        outputf = self.options["OutputFunction"]["Value"]
+        scriptEnd += " | {outputf} | ".format(outputf=outputf) + '%{$_ + \"`n\"};"`n'+str(moduleName)+' completed!"'
         script += scriptEnd
 
         if obfuscate:
